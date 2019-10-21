89e26e87dc3e8caa46b63f7bd8f4d313c94a427b
Fenzland <Fenzland@163.com>
2019-10-21 15:39:01 +0800

    use Laroute

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use function Laroute\route as laroute;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -51,9 +52,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+        laroute( base_path( 'routes/web' ) );
     }
 
     /**
@@ -65,9 +64,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+        laroute( base_path( 'routes/api' ) );
     }
 }
